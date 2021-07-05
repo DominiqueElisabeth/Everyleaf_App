f79f98267a3a0a52a4faf3df8bd4d906c00bded2
dominique owoua <domelisa@ymail.com>
2021-07-05 08:48:01 +0000

    step4-2

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :tasks
   resources :users
   resources :sessions
-  
+
   namespace :admin do
     resources :users, only: [:index, :show, :new, :create, :destroy, :edit, :update] do
     end
